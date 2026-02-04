cb3bb6ba865e83b002c95bd190b819148f4a4428
Amiel Platino <laplatino27@gmail.com>
2026-02-04 16:40:49 +0800

    Add read item endpoints

--- app/crud.py
+++ app/crud.py
@@ -1,4 +1,5 @@
-from sqlmodel import Session
+from typing import List, Optional
+from sqlmodel import Session, select
 
 from .models import Item, ItemCreate
 
@@ -8,3 +9,11 @@ def create_item(session: Session, item_create: ItemCreate) -> Item:
     session.commit()
     session.refresh(item)
     return item
+
+def get_item(session: Session, item_id: int) -> Optional[Item]:
+    return session.get(Item, item_id)
+
+def list_items(session: Session, limit: int = 100) -> List[Item]:
+    statement = select(Item).limit(limit)
+    results = session.exec(statement)
+    return results.all()
--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from sqlmodel import Session
 from .database import test_db_connection, create_db_and_tables, get_session
 from . import models
 from .models import ItemCreate, ItemRead
-from .crud import create_item
+from .crud import create_item, get_item, list_items
 
 app = FastAPI()
 
@@ -25,3 +25,14 @@ def health_db(session: Session = Depends(get_session)):
 def create_item_route(item: ItemCreate, session: Session = Depends(get_session)):
     created = create_item(session, item)
     return created
+
+@app.get("/items", response_model=list[ItemRead])
+def list_items_rout(limit: int = 100,session: Session = Depends(get_session)):
+    return list_items(session, limit=limit)
+
+@app.get("/items/{item_id}", response_model=ItemRead)
+def get_item_route(item_id: int, session: Session = Depends(get_session)):
+    item = get_item(session, item_id)
+    if not item:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return item
